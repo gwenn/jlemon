c8c2b9da54148b9b976fb3350339618f93df134a
gwenn <gtreguier@gmail.com>
2017-05-27 09:58:15 +0200

    Some more tests

--- src/test/java/simple/TestSimple.java
+++ src/test/java/simple/TestSimple.java
@@ -16,8 +16,29 @@ public class TestSimple {
         p.Parse(TIMES, 0, r);
         p.Parse(INTEGER, 4, r);
         p.Parse(0, 0, r);
-        p.ParseFinalize();
-
         assertEquals("(5 PLUS (10 TIMES 4))", r.expr.String());
+
+        //r = new Result();
+        p.Parse(INTEGER, 15, r);
+        p.Parse(DIVIDE, 0, r);
+        p.Parse(INTEGER, 5, r);
+        p.Parse(0, 0, r);
+        assertEquals("(15 DIVIDE 5)", r.expr.String());
+
+        p.Parse(INTEGER, 50, r);
+        p.Parse(PLUS, 0, r);
+        p.Parse(INTEGER, 125, r);
+        p.Parse(0, 0, r);
+        assertEquals("(50 PLUS 125)", r.expr.String());
+
+        p.Parse(INTEGER, 50, r);
+        p.Parse(TIMES, 0, r);
+        p.Parse(INTEGER, 125, r);
+        p.Parse(PLUS, 0, r);
+        p.Parse(INTEGER, 125, r);
+        p.Parse(0, 0, r);
+        assertEquals("((50 TIMES 125) PLUS 125)", r.expr.String());
+
+        p.ParseFinalize();
     }
 }
