710d1dd8a7ca586e26a643d8ae148b3b90552add
gwenn <gtreguier@gmail.com>
2017-07-02 19:22:53 +0200

    Auto generate parser on maven build

--- parser.sh
+++ parser.sh
@@ -1,2 +1,4 @@
-./jlemon -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT src/main/java/org/sqlite/parser/parse.y
-cpp -P src/main/java/org/sqlite/parser/parse.j > src/main/java/org/sqlite/parser/yyParser.java
+DIR=${1:-$(dirname "$0")}
+echo $DIR
+$DIR/jlemon -DSQLITE_ENABLE_UPDATE_DELETE_LIMIT $DIR/src/main/java/org/sqlite/parser/parse.y
+cpp -P $DIR/src/main/java/org/sqlite/parser/parse.j > $DIR/src/main/java/org/sqlite/parser/yyParser.java
