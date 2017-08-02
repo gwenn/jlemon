13f8be9ddf498077656f160d5a932a914025c85c
gwenn <gtreguier@gmail.com>
2017-08-02 21:56:44 +0200

    Load sql scripts recursively

--- src/test/java/org/sqlite/parser/Main.java
+++ src/test/java/org/sqlite/parser/Main.java
@@ -4,9 +4,13 @@ import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 import java.nio.file.DirectoryStream;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 
 import org.sqlite.parser.ast.Cmd;
 
@@ -18,11 +22,18 @@ class Main {
 
 	public static void main(String[] args) throws IOException {
 		Path dir = Paths.get(args[0]);
-		try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.{sql}")) {
+		Files.walkFileTree(dir, new SimpleFileVisitor<Path>() {
+			@Override
+			public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+				parse(file);
+				return super.visitFile(file, attrs);
+			}
+		});
+		/*try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.{sql}")) {
 			for (Path path : stream) {
 				parse(path);
 			}
-		}
+		}*/
 	}
 	public static void main_(String[] args) throws IOException {
 		Path dir = Paths.get(args[0]);
