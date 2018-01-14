b4e4ed45d6c4c734ff3df1fbb0d528ef11df99a4
gwenn <gtreguier@gmail.com>
2018-01-14 10:28:39 +0100

    Misc

--- src/test/java/simple/TokenType.java
+++ src/test/java/simple/TokenType.java
@@ -11,14 +11,14 @@ public interface TokenType {
   short DIVIDE                         =   3;
   short TIMES                          =   4;
   short INTEGER                        =   5;
-  static String toString(short token) {
-    switch(token) {
+  static String toString(short tokenType) {
+    switch(tokenType) {
     case 1  : return "PLUS";
     case 2  : return "MINUS";
     case 3  : return "DIVIDE";
     case 4  : return "TIMES";
     case 5  : return "INTEGER";
     }
-    throw new AssertionError(String.format("Unexpected token: %d", token));
+    throw new AssertionError(String.format("Unexpected token type: %d", tokenType));
   }
 }
